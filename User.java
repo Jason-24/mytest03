358346f456f96e1926eb18409fe0e8e5cda3df38
github's Name <github@xx.com>
2020-07-22 11:15:19 +0800

    修改

--- User.java
+++ User.java
@@ -3,7 +3,7 @@ package com.itheima.pojo;
 public class User {
     private String username;
     private int age;
-
+    private int birthday;
 
     public String getUsername() {
         return username;
