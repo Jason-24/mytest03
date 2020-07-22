93bc4577905bd299b3a59cb68ca1506511dd2230
github's Name <github@xx.com>
2020-07-22 11:28:12 +0800

    胜多负少

--- User.java
+++ User.java
@@ -8,7 +8,9 @@ public class User {
 56464564564
     private int birthday;
     private String password;
-
+534664
+564645
+456455+646
     public String getUsername() {
         return username;
     }
